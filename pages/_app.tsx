d2ca4b1b45704e7e2956b3af003e0e62c9c4f98d
Arnau Gómez <agomez@mrmilu.com>
2021-04-06 12:52:13 +0200

    extended eslintrc config to javascript files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
-import 'tailwindcss/tailwind.css';
-import Header from '../components/common/Header';
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import Header from "../components/common/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -8,18 +8,15 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
