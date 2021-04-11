bb0b318a8dac97372c9d5c25791ca360c401f2f1
Arnau Gómez Farell <agomez1f@gmail.com>
2021-04-11 21:34:01 +0200

    feat(header mobile): cool hamburger menu animation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,15 @@
 import MaxWidth from "./atoms/MaxWidth";
 import Navbar from "./Navbar";
+import Link from "next/link";
 
 const Header = (): JSX.Element => (
   <header className="fixed inset-x-0 p-5 bg-green text-white">
     <MaxWidth>
       <div className="flex justify-between">
         <h1 className="sr-only">Devs for Change</h1>
-        <div className="font-mono text-xl">{">_ Devs for Change"}</div>
+        <Link href="/">
+          <div className="font-mono text-xl">{">_ Devs for Change"}</div>
+        </Link>
         <Navbar />
       </div>
     </MaxWidth>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
 import cn from "classnames";
 import Link from "next/link";
+import { Squash as Hamburger } from "hamburger-react";
+import { useState } from "react";
 
 const links = [
   {
@@ -16,23 +18,37 @@ const links = [
   },
 ];
 
-const Navbar = (): JSX.Element => (
-  <nav>
-    <div className="hidden sm:flex justify-end items-center space-x-5 uppercase">
-      {links.map(({ label, href }, index) => (
-        <div
-          key={label}
-          className={cn(
-            "px-3 py-0.5",
-            index === links.length - 1 && "bg-white text-green font-bold"
-          )}
-        >
-          <Link href={href}>{label}</Link>
+const Navbar = (): JSX.Element => {
+  const [open, setOpen] = useState<boolean>(false);
+  const toggle = (): void => setOpen(!open);
+
+  return (
+    <nav>
+      <div
+        className={cn(
+          "overflow-hidden transition-all sm:overflow-auto absolute sm:relative top-16 sm:top-auto inset-x-0 sm:inset-x-auto",
+          !open ? "h-0 sm:h-auto" : "h-80 sm:h-auto"
+        )}
+      >
+        <div className="justify-end bg-green uppercase items-center p-7 pb-10 sm:pb-0 sm:p-0 space-y-10 sm:space-y-0 sm:space-x-7 flex flex-col sm:flex-row ">
+          {links.map(({ label, href }, index) => (
+            <div
+              key={label}
+              className={cn(
+                "py-0.5",
+                index === links.length - 1 && "px-3 bg-white text-green font-bold"
+              )}
+            >
+              <Link href={href}>{label}</Link>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-    <div className="sm:hidden">Hamburger</div>
-  </nav>
-);
+      </div>
+      <div className="absolute inset-y-0 right-0 flex items-center pr-5 sm:hidden">
+        <Hamburger onToggle={toggle} />
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
