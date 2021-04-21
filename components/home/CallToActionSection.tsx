aa653802aa1c623096dfd2252ba5b3bcaebad59b
Arnau Gómez <agomez@mrmilu.com>
2021-04-21 08:31:31 +0200

    feat(signup page): created new page and added links to it

--- components/home/CallToActionSection.tsx
+++ components/home/CallToActionSection.tsx
@@ -2,6 +2,7 @@ import MaxWidth from "../atoms/MaxWidth";
 import Section from "../atoms/Section";
 import TextH2 from "../atoms/TextH2";
 import Button from "../atoms/Button";
+import Link from "next/link";
 
 const CallToActionSection = (): JSX.Element => (
   <Section>
@@ -9,8 +10,12 @@ const CallToActionSection = (): JSX.Element => (
       <div className="py-20">
         <TextH2 className="pb-14 text-center">Sign up now</TextH2>
         <div className="flex flex-wrap justify-center items-center gap-8">
-          <Button>Start as a developer</Button>
-          <Button style="inverted">I am a non-profit</Button>
+          <Link href="/signup">
+            <Button>Start as a developer</Button>
+          </Link>
+          <Link href="/signup">
+            <Button style="inverted">I am a non-profit</Button>
+          </Link>
         </div>
       </div>
     </MaxWidth>
